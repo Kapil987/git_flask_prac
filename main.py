e97af674ac038377559a1d61b655b4599496b98c
Kapil <kapil0123@gmai.com>
2026-04-05 20:16:16 +0530

    i have code to run the application

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def main():
-    print("Hello from test-git!")
+from flask import Flask
 
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
 
 if __name__ == "__main__":
-    main()
+    app.run(host='0.0.0.0', port=5000)
